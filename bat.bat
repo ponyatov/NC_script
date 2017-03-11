fec5842b2d957b7041907779e6795d53dabe6ebc
Dmitry Ponyatov <dponyatov@gmail.com>
2017-03-11 13:06:42 +0400

    + test.nc
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bat.bat
+++ bat.bat
@@ -1,1 +1,1 @@
-@gvim -p test.ncs test.log ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile .gitignore bat.bat README.md
+@gvim -p test.ncs test.nc ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile .gitignore bat.bat README.md
